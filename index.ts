dee372caf1a2bb8c164f97e5e6c50452d2b255c9
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2023-09-23 14:12:31 +0900

    feat: add hono

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+import { serveStatic } from 'hono/bun';
+
+const app = new Hono();
+
+app.use('/*', serveStatic({ root: './public' }))
+app.get("/", (c) => c.text("Hono!!"));
+
+export default app;
