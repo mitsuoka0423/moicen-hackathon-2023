b7c05a924848ef13070b89c15afc42b0ebe04e46
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2023-09-23 17:13:27 +0900

    feat: add reservation

--- src/routes/reservation.ts
+++ src/routes/reservation.ts
@@ -1,13 +1,21 @@
 import { Context } from "hono";
 
-export const post = (c: Context) => {
+export const post = async (c: Context) => {
   const url = new URL(c.req.url);
 
-  return c.json({
+  const requestBody = await c.req.json();
+  console.log({ request: requestBody });
+
+  const responseBody = {
     result: "ok",
     numberedTicket: {
       id: "R001",
-      imageUrl: `${url.origin}/1695452728.png`,
+      // imageUrl: `${url.origin}/1695452728.png`,
+      imageUrl: `https://cvm9vgmd-3000.asse.devtunnels.ms/1695452728.png`,
     },
-  });
+  };
+
+  console.log({ response: responseBody });
+
+  return c.json(responseBody);
 };
